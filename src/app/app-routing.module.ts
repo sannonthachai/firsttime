cc7c69bfb7709a92ce7b80c31eccf5f25d51bb5a
sannonthachai <sannonthachai@gmail.com>
2019-07-30 16:41:16 +0700

    Create UI update customer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InputUserDataFormComponent } from './input-user-data-form/input-user-data-form.component';
 import { DisplayUserDataComponent } from './display-user-data/display-user-data.component';
+import { UpdateUserDataComponent } from './update-user-data/update-user-data.component'
 
 const routes: Routes = [
       {path: '', component: DisplayUserDataComponent},
       {path: 'customer', component: InputUserDataFormComponent},
+      {path: 'update', component: UpdateUserDataComponent},
       {path: '**', redirectTo: '', pathMatch: 'full'}
     ];
 
